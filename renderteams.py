28d7921ad08c4a77ca89bc9e2533cb88ded830f7
prasadthorati <91524881+prasadthorati@users.noreply.github.com>
2026-07-08 13:01:13 +0530

    Change server port from 5000 to 8080

--- renderteams.py
+++ renderteams.py
@@ -922,5 +922,5 @@ def download_discovered():
 
 
 if __name__ == "__main__":
-    print("Starting UI... open http://127.0.0.1:5000")
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    print("Starting UI... open http://127.0.0.1:8080")
+    app.run(host="127.0.0.1", port=8080, debug=False)
